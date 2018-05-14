44b3ef6bce27362f9f970c877c1baed738e635e1
h3r0xfr <adrien.cabaud@numericable.fr>
2018-05-14 09:37:19 +0200

    error correction

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 setInterval(() => {
     fs.appendFile("time.txt", Date.now() + "\r\n", (error) => {
-        console.log("Erreur lors de l'écriture du timestamp : " + error);
+        if(error) return console.log("Erreur lors de l'écriture du timestamp : " + error);
     });
 }, 1000);
